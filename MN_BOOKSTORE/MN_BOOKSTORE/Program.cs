0a5b62ef1140a66b4652e9545519b03bc8affce8
MrSimple263 <quangminhtran26@gmail.com>
2018-04-15 22:02:05 +0700

    form hóa đơn

--- MN_BOOKSTORE/MN_BOOKSTORE/Program.cs
+++ MN_BOOKSTORE/MN_BOOKSTORE/Program.cs
@@ -16,7 +16,7 @@ namespace MN_BOOKSTORE
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
--- MN_BOOKSTORE/MN_BOOKSTORE/TaoHoaDon.cs
+++ MN_BOOKSTORE/MN_BOOKSTORE/TaoHoaDon.cs
@@ -10,11 +10,13 @@ using System.Windows.Forms;
 
 namespace MN_BOOKSTORE
 {
-    public partial class Form1 : Form
+    public partial class TaoHoaDon : Form
     {
-        public Form1()
+        public TaoHoaDon()
         {
             InitializeComponent();
         }
+
+       
     }
 }
