520357d457cf916a0103d53bb12aecdeeb854a89
MrSimple263 <quangminhtran26@gmail.com>
2018-04-18 22:54:39 +0700

    class connection không có push lên vì mỗi máy có mỗi server khac nhau

---
+++ MN_BOOKSTORE/MN_BOOKSTORE/DAO/Connection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace MN_BOOKSTORE.DAO
+{
+    class Connection
+    {
+        SqlConnection sqlConnection;
+        SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+        public Connection()
+        {
+
+            sqlConnectionStringBuilder.DataSource = "MINH";
+            sqlConnectionStringBuilder.InitialCatalog = "CUAHANGSACH";
+            sqlConnectionStringBuilder.UserID = "minh";
+            sqlConnectionStringBuilder.Password = "123";
+            sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ToString());
+            try
+            {
+                sqlConnection.Open();
+                Debug.Write("kết nối thành công");
+
+            }
+            catch (SqlException ex)
+            {
+                Debug.Write(ex.Message);
+            }
+
+        }
+    }
+}
