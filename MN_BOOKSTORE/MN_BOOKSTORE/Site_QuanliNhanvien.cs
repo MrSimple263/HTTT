814bbef04813ef45f9e858c09537017f7d892133
MrSimple263 <quangminhtran26@gmail.com>
2018-06-17 15:13:00 +0700

    bản thử 2

--- MN_BOOKSTORE/MN_BOOKSTORE/Site_QuanliNhanvien.cs
+++ MN_BOOKSTORE/MN_BOOKSTORE/Site_QuanliNhanvien.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 using BLL;
-using System.Diagnostics;
 namespace MN_BOOKSTORE
 {
     public partial class Site_QuanliNhanvien : Form
@@ -50,32 +42,6 @@ namespace MN_BOOKSTORE
             }
             dgv_Nhanvien.DataSource = nhanviens;
             dgv_Nhanvien.Columns[6].Visible = false;
-            /*
-        DataTable site1 = new DICTON_BLL().selectid(2);
-        foreach (DataRow row in site1.Rows)
-        {
-            DAL.DAL.datasource = row["IPADDRESS"].ToString();
-            DAL.DAL.userid = row["ACCOUNT"].ToString();
-            DAL.DAL.pass = row["PASS"].ToString();
-            DAL.DAL.catalog = row["NAMEDATABASE"].ToString();
-        }
-        NhanVien_BLL nhanvienhn = new NhanVien_BLL();
-        hn = nhanvienhn.getall();
-        DataTable site2 = new DICTON_BLL().selectid(1);
-        foreach (DataRow row in site2.Rows)
-        {
-            DAL.DAL.datasource = row["IPADDRESS"].ToString();
-            DAL.DAL.userid = row["ACCOUNT"].ToString();
-            DAL.DAL.pass = row["PASS"].ToString();
-            DAL.DAL.catalog = row["NAMEDATABASE"].ToString();
-        }
-        NhanVien_BLL nhanvienhcm = new NhanVien_BLL();
-        hcm = nhanvienhcm.getall();
-        DataTable dtall = new DataTable();
-        dtall = hcm.Copy();
-        dtall.Merge(hn);
-        dgv_Nhanvien.DataSource = dtall;
-        */
         }
 
         private void dgv_Nhanvien_CellClick(object sender, DataGridViewCellEventArgs e)
